2173058a0948392b8d5d15c238d896364072bc26
DJCoder311 <166066300+DJCoder311@users.noreply.github.com>
2025-11-27 19:32:44 -0500

    Completed Assignment

--- src/main/java/exercises/AdvanceSetInterface.java
+++ src/main/java/exercises/AdvanceSetInterface.java
@@ -1,5 +1,6 @@
 package exercises;
 
+import com.sun.source.doctree.SeeTree;
 import edu.touro.mcon264.apps.collections.CollectionInterface;
 
 /**
@@ -7,12 +8,9 @@ import edu.touro.mcon264.apps.collections.CollectionInterface;
  * @param <T>
  */
 public interface AdvanceSetInterface <T> extends CollectionInterface<T> {
-/*
- Starter code the exercise 6
- Desig signatures for the following methods:
-    union()
-    intersection()
-    difference()
- */
+
+    public AdvanceSetInterface<T> union(AdvanceSetInterface<T> otherSet);
+    public AdvanceSetInterface<T> intersection(AdvanceSetInterface<T> otherSet);
+    public AdvanceSetInterface<T> difference(AdvanceSetInterface<T> otherSet);
 
 }
--- src/main/java/exercises/ExtendedLinkedCollection.java
+++ src/main/java/exercises/ExtendedLinkedCollection.java
@@ -1,19 +1,48 @@
 package exercises;
 
 import edu.touro.mcon264.apps.collections.LinkedCollection;
+import edu.touro.mcon264.support.LLNode;
 
 public class ExtendedLinkedCollection<T> extends LinkedCollection<T> {
+
+    //protected LLNode<T> head;
     @Override
     public String toString() {
-      //ТОDO: implement this method. Use StringBuider. Create Javadoc.
+        LLNode<T> current = head;
+        StringBuilder result = new StringBuilder();
+        while(current != null){
+            result.append(current.getInfo());
+            current = current.getLink();
+        }
+        return result.toString();
     }
 
 
     public int count(T target) {
-      //ТОDO: implement this method. Count the number of times target appears in the collection. Create Javadoc.
+        LLNode<T> current = head;
+        int totalMatches = 0;
+        while(current!=null){
+            if(current.getInfo() == target){
+                totalMatches++;
+            }
+            current = current.getLink();
+        }
+        return totalMatches;
     }
 
     public void removeAll(T target) {
-      //ТОDO: implement this method. Remove all occurrences of target from the collection and return the number of elements removed. Create Javadoc.
+        LLNode<T> current = head;
+        LLNode<T> prev = current;
+        while(current != null){
+            if(head.getInfo() == target){
+                head.setLink(head.getLink());
+                head = head.getLink();
+            }
+            else if(current.getInfo() == target){
+                prev.setLink(current.getLink());
+            }
+            prev = current;
+            current = current.getLink();
+        }
     }
 }
